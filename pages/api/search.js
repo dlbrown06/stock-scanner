9f59956c1464cd5b5aa10110abafde711f2fe1de
Doug Brown <douglas_l_brown@apple.com>
2020-12-08 18:35:40 -0500

    Main page, search by symbol

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,6 +1,10 @@
 const alpha = require('alphavantage')({ key: process.env.ALPHA_VANTAGE_KEY });
 
 export default async function handler(req, res) {
-  const search = await alpha.data.search('apple');
-  res.status(200).json(search.bestMatches)
+  try {
+    const search = await alpha.data.search(req.query.keywords);
+    res.status(200).json(search.bestMatches);
+  } catch (err) {
+    res.status(429).json({ data: [], error: err });
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useState } from "react";
 import useSWR from 'swr';
 import styled from "styled-components";
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
-
-  const SymbolSearchInput = styled.input`
+// Styled Components
+const SymbolSearchInput = styled.input`
     padding: 0.5em;
     margin: 0.5em;
     color: darkgray;
@@ -16,11 +16,12 @@ export default function Home() {
     text-transform: uppercase;
   `;
 
-  const fetchSymbol = () => {
-    const { data, error } = useSWR('/api/stock', fetch);
-  };
+export default function Home() {
+  const [searchSymbol, setSearchSymbol] = useState("");
 
-  const { data, error } = useSWR('/api/search?keywords=apple', fetch);
+  const fetcher = url => fetch(url).then(r => r.json())
+  const fetchRsp = useSWR(`/api/search?keywords=${searchSymbol}`, fetcher);
+  const { data } = fetchRsp;
 
   return (
     <div className={styles.container}>
@@ -39,7 +40,10 @@ export default function Home() {
         <p className={styles.description}>
           Get started by searching a stock symbol
         </p>
-        <SymbolSearchInput type="text" onClick={() => fetchSymbol()} />        
+        <p><SymbolSearchInput type="text" onKeyUp={e => setSearchSymbol(e.target.value.toUpperCase())} /></p>
+        <ul>
+          {Array.isArray(data) ? data.map((symbolObj, key) => <li key={key}>{symbolObj["1. symbol"]} - {symbolObj["2. name"]}</li>) : null}
+        </ul>
       </main>
 
       <footer className={styles.footer}>
